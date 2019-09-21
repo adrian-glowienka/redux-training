c3f779505b63d7eab3193235990ba94e2064cd90
adisonn88 <adisonn88@wp.pl>
2019-09-21 09:46:04 +0200

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import React from 'react';
-import Todos from './components/Todos'
-import AddTodo from './components/AddTodo'
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
-  state = {
-    todos: [
-      {id:1, content: 'buy milk'},
-      {id:2, content: 'go to work'}
-    ]
-  }
-
-  deleteTodo = (id) => {
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id
-    });
-    this.setState({
-      todos
-    })
-  }
-  addTodo = (todo) => {
-    todo.id = Math.random();
-    let todos  = [...this.state.todos, todo];
-    this.setState({
-      todos
-    })
-  }
-  render() {
+  render(){
     return (
       <div className="App">
-        <h1 className="center blue-text">Todo List</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-        <AddTodo addTodo={this.addTodo}/>
+        <Navbar />
       </div>
     )
   }
-
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>NASA was established in 1958, succeeding the National Advisory Committee for Aeronautics (NACA). The new agency was to have a distinctly civilian orientation, encouraging peaceful applications in space science.[7][8][9] Since its establishment, most US space exploration efforts have been led by NASA, including the Apollo Moon landing missions, the Skylab space station, and later the Space Shuttle. NASA is supporting the International Space Station and is overseeing the development of the Orion Multi-Purpose Crew Vehicle, the Space Launch System and Commercial Crew vehicles. The agency is also responsible for the Launch Services Program which provides oversight of launch operations and countdown management for unmanned NASA launches.</p>
+        </div>
+    )
+}
+
+export default About;
