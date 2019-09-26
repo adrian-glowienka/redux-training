de5fb4b8acff58d5264ea3a9c1c34e5576d40c1b
adrian-glowienka <45940806+adrian-glowienka@users.noreply.github.com>
2019-09-26 10:58:21 +0200

    blog detail page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import axios from 'axios'
+import { connect } from 'react-redux'
 
-export default class Post extends React.Component {
-    state = {
-        id: null
-    }
-    componentDidMount(){
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' +id)
-            .then(res =>{
-                this.setState({
-                    post: res.data
-                })
-            })
-        this.setState({
-            id
-        })
-    }
+class Post extends React.Component {
+
     render(){
 
-        const post = this.state.post ? (
+        const post = this.props.post ? (
             <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                <h4 className="center">{this.props.post.title}</h4>
+                <p>{this.props.post.body}</p>
             </div>
         ):(
             <div className="center">Loeading post ...</div>
@@ -35,3 +21,12 @@ export default class Post extends React.Component {
         )
     }
 }
+
+const mapStatetoProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    return {
+        post: state.posts.find(post => post.id === id)
+        }
+}
+
+export default connect(mapStatetoProps)(Post)
